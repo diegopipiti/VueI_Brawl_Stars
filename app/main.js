61ba8bc3479b8bc0dbe3d164585bb4fa1a2b0459
marcelloburattini79 <burattinimarcello@yahoo.it>
2020-03-27 11:02:11 +0100

    main.js ok

---
+++ app/main.js
@@ -0,0 +1,19 @@
+new Vue({
+    el: '#app',
+    
+    data: {
+        submissions: Seed.submissions
+    },
+    
+    computed: {
+        sortedSubmissions () {
+            return this.submissions.sort((a, b) => {
+            return b.votes - a.votes
+        });
+    }
+},
+
+    components: {
+        'submission-component': submissionComponent
+    }
+});
